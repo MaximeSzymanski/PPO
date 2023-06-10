569c5fb9a84a585c46f51e21d9f3befb4ab96b5d
maximeszymanski <2maxime.szymanski@gmail.com>
2023-06-10 18:27:27 +0200

    Changing parsing

--- src/training.py
+++ src/training.py
@@ -7,7 +7,7 @@ def start_training():
 
     """
     # Define the parser
-    continuous, actor_hidden_size, critic_hidden_size, lr, gamma, K_epochs, eps_clip, entropy_coef, value_loss_coef, gae_lambda, max_timesteps_one_episode, timestep_per_update, env_name, recurrent, decay_rate, minibatch_size, render, save_freq = get_hyperparameters()
+    continuous, actor_hidden_size, critic_hidden_size, lr, gamma, K_epochs, eps_clip, entropy_coef, value_loss_coef, gae_lambda, max_timesteps_one_episode, timestep_per_update, env_name, recurrent, decay_rate, minibatch_size, render, save_freq, shapley_value,class_name,features_name = get_hyperparameters()
     PPO = PPOFactory.create_PPO(continuous=continuous, actor_hidden_size=actor_hidden_size,
                                 critic_hidden_size=critic_hidden_size, lr=lr, gamma=gamma,
                                 eps_clip=eps_clip, entropy_coef=entropy_coef, value_loss_coef=value_loss_coef,
@@ -15,7 +15,7 @@ def start_training():
                                 recurrent=recurrent,
                                 decay_rate=decay_rate, minibatch_size=minibatch_size,
                                 timestep_per_episode=max_timesteps_one_episode,
-                                epochs=K_epochs, render=render)
+                                epochs=K_epochs, render=render,shapley_values=shapley_value,class_name=class_name,features_name=features_name)
 
     print(PPO)
 
